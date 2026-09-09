c9385956000bd232c281401523c0bd4709dd82c5
notkay888 <gaomichaela@gmail.com>
2026-09-09 18:01:43 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,7 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.println("I love to learn coding remotely.");
-		System.out.println("");
+		String happy = new String("Happy Birthday to you");
+		String birthday = new String("Happy birthday to you");
+		String you = new String("Happy birthday dear somebody");
+		System.out.println(happy);
+		System.out.println(birthday);
+		System.out.println(you);
+		System.out.println(birthday);
 	}
 }
--- Java/000_Labs/005_int/base_code/starter.java
+++ Java/000_Labs/005_int/base_code/starter.java
@@ -9,6 +9,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int one = 5*7/2%3-1; 
+		int two = 10%3-4*7+2;
+		int three = (3-7/2*5)%10;
+		System.out.println(one);
+		System.out.println(two);
+		System.out.println(three);
 	}
 }
