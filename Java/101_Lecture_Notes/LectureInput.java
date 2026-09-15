87b9b5e9ec6d145df31e38ab90ef7af7e8d462ba
notkay888 <gaomichaela@gmail.com>
2026-09-15 17:57:23 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,30 @@
 /*
     Lecture note example - Input!!
 */
+import java.util.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+        double shrimpTempura = 4.75;
+        double misoSoup = 2.50;
+        double softDrink = 1.25;
+        System.out.println("Welcome to Michaela's Sushi Bar");
+        System.out.println("Here's our menu:");
+        System.out.print("1. Shrimp Tempura - " + "$");
+        System.out.println(shrimpTempura);
+
+        System.out.print("2. Miso Soup - " + "$");
+        System.out.println(misoSoup);
+        
+        System.out.print("3. Soft Drink - " + "$");
+        System.out.println(softDrink);
+
+
+
+        System.out.println("How many Shrimp Tempuras would you like?");
+        Scanner sc = new Scanner(System.in);
+
+        int item1Quant = sc.nextInt();
+        System.out.println("Item 1 Total is: " + (shrimpTempura*item1Quant));
 	}
 }
